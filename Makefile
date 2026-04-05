6179954b934f42c469bb4a9df31873bb152d3faa
Dorian Abdullahi <dabdulla@c2r1p9.42vienna.com>
2026-04-05 17:03:33 +0200

    Added a few files and getting everything ready

---
+++ Makefile
@@ -0,0 +1,50 @@
+NAME = push_swap
+
+SOURCE = src/push_swap.c \
+			src/moves.c
+
+HEADER = push_swap.h
+
+CFLAGS = -Wall -Wextra -Werror -g -fsanitize=address
+
+
+OBJECTS = $(SOURCE:.c=.o)
+
+LIBFT_DIR = include/libft
+FT_PRINTF_DIR = include/ft_printf
+
+LIBFT = ${LIBFT_DIR}/libft.a
+FT_PRINTF = ${FT_PRINTF_DIR}/libftprintf.a
+
+CC = cc
+RM = rm -f
+
+all: ${NAME}
+
+$(LIBFT):
+		@make -C $(LIBFT_DIR)
+
+$(FT_PRINTF):
+		@make -C $(FT_PRINTF_DIR)
+
+%.o: %.c push_swap.h libft/libft.h ft_printf/ft_printf.h
+	${CC} ${CFLAGS} -c $< -o $@
+
+${NAME}: ${LIBFT} ${FT_PRINTF} ${OBJECTS}
+	${CC} ${CFLAGS} ${OBJECTS} ${LIBFT} ${FT_PRINTF} -o ${NAME}
+
+clean:
+	make -C ${LIBFT_DIR} clean
+	make -C ${FT_PRINTF_DIR} clean
+	${RM} ${OBJECTS}
+
+fclean: clean
+	make -C ${LIBFT_DIR} fclean
+	make -C ${FT_PRINTF_DIR} fclean
+	${RM} ${NAME}
+
+re: fclean all
+
+make: make all
+
+.PHONY: clean fclean re all
---
+++ src/push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabdulla <dabdulla@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/01 10:42:54 by dabdulla          #+#    #+#             */
+/*   Updated: 2026/04/05 17:00:58 by dabdulla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "../include/libft/libft.h"
+# include "../include/ft_printf/ft_printf.h"
+# include <limits.h>
+
+#endif
